114d78e4df08b43dfd6ba22fa9de7a6541339b7c
Tushar <tushar8133@gmail.com>
2024-02-23 23:34:51 +0530

    static server changes

--- server.js
+++ server.js
@@ -1,83 +1,63 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 const fse = require('fs-extra');
 const path = require('path');
+const config = require('./config.js');
 
-let port = null;
-let folder = null;
-
-try {
-    const args = process.argv.slice(2);
-	port = args.find((item) => item.includes('--port')).split('=')[1];
-	folder = args.find((item) => item.includes('--folder')).split('=')[1];
-	if (!port || !folder) {
-		throw new Error();
-	}
-} catch (e) {
-    console.log(`Something's wrong! Try like this: \x1b[7m> node server.js --port=8080 --folder=ABC\x1b[0m`);
-    return;
-}
+const folderName = config.HAR_FILE.replace(/\.har$/, '');
+const folder = path.join(config.DEFAULT_PATH, folderName);
 
 app.use(cors());
-app.use(express.json()); // get read the response body
-app.use('/', express.static('./public'));
+app.use(express.json());
 
-
-const ignoreExt = ['.ico', '.svg', '.jpg', '.gif', '.font' ];
-let delayCounter = 0;
-
-app.all("*", (req, res) => {
+app.all('*', (req, res) => {
     try {
 
-		// const url = require("url");
+		// const url = require('url');
 		// let pathname = url.parse(req.url).pathname;
-		const resource = req.path.split('/').pop();
-		const badItem = ignoreExt.some((item) => resource.includes(item));
-		if (badItem) {
-			res.end();
-			console.log("Resource filtered out >>>", resource);
-			return false;
-			/**
-			 * Below ones are useful in socket connection
-			 * // response.set("Connection", "close");
-			 * // response.connection.end();
-			 * // response.connection.destroy();
-			 */
+		let resource = req.path.replace(/\/$/, '').split('/').pop();
+
+		if (resource.includes('.')) {
+			const fullPath = path.join(folder, resource);
+			const isExist = fse.pathExistsSync(fullPath);
+			if (!isExist) {
+				res.status(404).end();
+				return;
+				/**
+				 * Below ones are useful in socket connection
+				 * // response.set('Connection', 'close');
+				 * // response.connection.end();
+				 * // response.connection.destroy();
+				 */
+			}
+			req.url = resource;
+			express.static(folder)(req, res);
+			return;
 		}
-		
-		let fileName = (resource.toLowerCase() === "graphql") ? (req.body.operationName || '') : resource;
 
-		console.log('>>', fileName);
-		// console.log("GRAPHQL variables >>>", req.body.variables);
-		// console.log("GRAPHQL query >>>", req.body.query);
-
-		fileName = fileName.replace('.json', '');
-		let filePath = path.join(folder, fileName);
-		const fileData = fse.readJsonSync(filePath + ".json");
-
-		setTimeout(() => {
-			res.json(fileData);
-			--delayCounter;
-		}, ++delayCounter * 1000);
+		resource = (resource.toLowerCase() === 'graphql') ? (req.body.operationName || '') : resource;
+		// console.log('GRAPHQL variables >>>', req.body.variables);
+		// console.log('GRAPHQL query >>>', req.body.query);
+		const fullPath = path.join(folder, resource + '.json');
+		const isExist = fse.pathExistsSync(fullPath);
+		if (!isExist) {
+			res.status(404).json({ 'error': 'mock file not found at the location'});
+			return;
+		}
+		const fileData = fse.readJsonSync(fullPath);
+		res.json(fileData);
 
 	} catch(err) {
 
-		console.error("Mock file not found");
-		res.set('Content-Type', 'text/html');
-		res.send(Buffer.from(`
-			<h3>Mock file not found</h3>
-			<p>Check if file present inside folder <b>&sol;${folder}</b></p>
-			<code>${err}</code>
-		`));
-		/*
-		res.status(404).json({
-			"ERROR MESSAGE": "Mock file not found!",
-		});
-		*/
+		console.error(err);
+		res.status(404).end(err);
+		// res.set('Content-Type', 'text/html');
+		// res.send(Buffer.from(`<h6>${err}</h6>`));
+		// res.status(404).json({ 'ERROR MESSAGE': 'Mock file not found!' });
 	}
 })
 
-app.listen(port, () => {
-	console.log("\x1b[7m%s", `http://localhost:${port} *** ${folder}`, '\x1b[0m');
+app.listen(config.PORT, () => {
+	console.log('\x1b[7m%s', `http://localhost:${config.PORT} from '${folder}'`, '\x1b[0m');
 });
