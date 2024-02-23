a5ab503a1721913efb4f1eda97e6e67760e670ba
Tushar <tushar8133@gmail.com>
2024-02-23 23:42:07 +0530

    config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const PORT = 3000;
+
+const HAR_FILE = "8888";
+
+const ALLOW_MERGE = true;
+
+const VERBOSE = true;
+
+const DEFAULT_PATH = "./";
+
+const RESPONSE_DELAY = 0;
+
+module.exports = {
+    PORT,
+    HAR_FILE,
+    ALLOW_MERGE,
+    VERBOSE,
+    DEFAULT_PATH,
+    RESPONSE_DELAY
+}
